80f6ef07cd0c0aaf097e102f604b656ba8b2e52d
Jason <vivanaranja@gmail.com>
2018-04-11 15:31:22 -0700

    add jQuery

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-function on() {
-    document.getElementById('overlay').style.display = 'block';
-}
+(function ($) {
+    $(function () {
+        var skills = ["Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, ", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam", "At vero eos et accusamus et iusto odio dignissimos", "text4"],
+            counter = skills.length - 1,
+            previousSkill = $("#myGreetingSkills"),
+            arraylength = skills.length - 1;
 
-function off() {
-    document.getElementById('overlay').style.display = 'none';
-}
+        function display_skills() {
+            if (counter === arraylength) {
+                counter = 0;
+            }
+            else {
+                counter++;
+            }
+            previousSkill.html(skills[counter]);
+        }
+
+        display_skills();
+
+        setInterval(function () {
+            display_skills();
+        }, 2000);
+    });
+})(jQuery);
